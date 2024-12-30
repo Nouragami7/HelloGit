a731e2daf44d0f1082ebb0d7ecf85cf2b9c8d16d
Nouragami7 <https://github.com/Nouragami7>
2024-12-30 13:00:19 +0200

    Add HelloITI class with printHelloITI method

---
+++ HelloITI.java
@@ -0,0 +1,10 @@
+public class HelloITI {
+    public void printHelloITI() {
+        System.out.println("Hello, ITI!");
+    }
+
+    public static void main(String[] args) {
+        HelloITI helloITI = new HelloITI();
+        helloITI.printHelloITI();
+    }
+}
