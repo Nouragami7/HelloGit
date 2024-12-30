e068186fd1d89ef2ee13ca66440a9a79654be813
Nouragami7 <https://github.com/Nouragami7>
2024-12-30 13:09:58 +0200

    Add printHelloGit method to HelloITI class

--- HelloITI.java
+++ HelloITI.java
@@ -2,9 +2,13 @@ public class HelloITI {
     public void printHelloITI() {
         System.out.println("Hello, ITI!");
     }
+	 public void printHelloGit() {
+        System.out.println("Hello, Git!");
+    }
 
     public static void main(String[] args) {
         HelloITI helloITI = new HelloITI();
         helloITI.printHelloITI();
+		helloITI.printHelloGit();
     }
 }
