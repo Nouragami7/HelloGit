f18d849a89ca0024082d1250bc47127ed713102e
Nouragami7 <https://github.com/Nouragami7>
2024-12-30 12:58:21 +0200

    Add HelloWorld class with printHelloWorld method

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public void printHelloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printHelloWorld();
+    }
+}
