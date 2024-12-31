653961f071c606382c4e5ab5fb873f471287d73e
Mohamed Taj Eldin Ahmed <mohamedtajeldinai@gmail.com>
2024-12-31 14:46:47 +0200

    edite the print function

--- HelloITI.java
+++ HelloITI.java
@@ -1,9 +1,10 @@
 public class HelloITI {
     public void printHelloITI() {
-        System.out.println("Hello, ITI!");
-    }
+        System.out.println("Hello,   ITI!");
+    	System.out.println("Hello, Git")
+	}
 	 public void printHelloGit() {
-        System.out.println("Hello, Git!");
+        System.out.println("Hello,    Git!");
     }
 	
 	 public void printHello() {
